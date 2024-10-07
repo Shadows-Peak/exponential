df0d3ce10fe93a5bcc73da9e829e99983d54c621
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-10-07 20:03:04 +0000

    installed flask because im dumb (test 2)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/get_value')
 def get_value():
-    value = "Hello, World!"  # Your variable value
+    value = "Test 2 with flask actually installed in the terminal (im stupid)!"  # Your variable value
     return jsonify({'value': value})
 
 if __name__ == '__main__':
