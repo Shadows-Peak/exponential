8d583d81fdea8e928c7742a6a3b0746d5dbd6d6f
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-10-08 20:44:53 +0000

    definetely didnt rely on ai a lot for some tweaks because i couldnt be bothered to do it all again myself. Removed LargeNumber object class because im stupid and there was only a limit in Python 2. Extended the abbreviations list

--- python/framework/generalFramework.py
+++ python/framework/generalFramework.py
@@ -8,7 +8,7 @@ def onArgRaiseError(parameters, parameterNames, types, minValues, maxValues, exc
     Args:
         parameters (tuple): Tuple of all parameters.
         parameterNames (list): List of all parameter names.
-        types (list): List of all parameter types. (You can nest if a parameter can take multiple types) [0 -> str, 1 -> int, 2 -> float, 3 -> bool, 4 -> LargeNumber, 5 -> list, 6 -> dict, 7 -> tuple]
+        types (list): List of all parameter types. (You can nest if a parameter can take multiple types) [0 -> str, 1 -> int, 2 -> float, 3 -> bool, 4 -> list, 5 -> dict, 6 -> tuple]
         minValues (list): List containing the minimum values of each parameter.
         maxValues (list): List containing the maximum values of each parameter.
         exceptionValues (list): List containing the exception values of each parameter. (You can nest if a parameter can take multiple exception values)
@@ -24,22 +24,20 @@ def onArgRaiseError(parameters, parameterNames, types, minValues, maxValues, exc
         elif parameterType == 3:
             return(type(parameter) == bool)
         elif parameterType == 4:
-            return(type(parameter).__name__ == "LargeNumber")
-        elif parameterType == 5:
             return(type(parameter) == list)
-        elif parameterType == 6:
+        elif parameterType == 5:
             return(type(parameter) == dict)
-        elif parameterType == 7:
+        elif parameterType == 6:
             return(type(parameter) == tuple)
     def convertTypeToName(parameterType): # Function to convert the type(s) to a string
         if type(parameterType) == list: # If the parameter can take multiple types
             typeNameStr = " or " # Joining string
             typeNameList = [] # Initialize dummy list
             for i in range(len(parameterType)): # Iterate through the types
-                typeNameList.append(["a string", "an integer", "a float", "a boolean", "a LargeNumber", "a list", "a dictionary", "a tuple"][parameterType[i]]) # Append the type to the list
+                typeNameList.append(["a string", "an integer", "a float", "a boolean", "a list", "a dictionary", "a tuple"][parameterType[i]]) # Append the type to the list
             typeNameStr = typeNameStr.join(typeNameList) # Join the list into a string with a seperator of " or"
             return typeNameStr # Return the types as a string
-        return ["a string", "an integer", "a float", "a boolean", "a LargeNumber", "a list", "a dictionary", "a tuple"][parameterType] # Return the type as a string
+        return ["a string", "an integer", "a float", "a boolean", "a list", "a dictionary", "a tuple"][parameterType] # Return the type as a string
 
     for i in range(len(parameters)):
         if type(types[i]) == list: # If the parameter can take multiple types
@@ -51,29 +49,25 @@ def onArgRaiseError(parameters, parameterNames, types, minValues, maxValues, exc
                     raise TypeError(str(parameterNames[i])+" must be "+convertTypeToName(types[i])+".") # Raise the error
         elif not checkType(parameters[i],types[i]): # If the parameter is not the type (singular)
             raise TypeError(str(parameterNames[i])+" must be "+convertTypeToName(types[i])+".") # Raise the error
-        if type(parameters[i]).__name__ in ["int","float","LargeNumber"]: # If the parameter is an integer, float, or LargeNumber
-            if parameters[i] <= minValues[i]: # If the parameter is less than the minimum value
-                if not minValues[i]: # If the minimum value is defined as "False"
+        if type(parameters[i]).__name__ in ["int","float"]: # If the parameter is an integer or float
+            if parameters[i] < minValues[i]: # If the parameter is less than the minimum value
+                if minValues[i] is False: # If the minimum value is defined as "False"
                     continue # Skip the check
                 raise ValueError(str(parameterNames[i])+" must be greater than or equal to "+str(minValues[i])+".") # Raise the error
-            if parameters[i] >= maxValues[i]: # If the parameter is greater than the maximum value
-                if not maxValues[i]: # If the maximum value is defined as "False"
+            if parameters[i] > maxValues[i]: # If the parameter is greater than the maximum value
+                if maxValues[i] is False: # If the maximum value is defined as "False"
                     continue # Skip the check
                 raise ValueError(str(parameterNames[i])+" must be less than or equal to "+str(maxValues[i])+".") # Raise the error
-            if parameters[i] in exceptionValues[i]: # If the parameter is an exception value
-                if not exceptionValues[i]: # If the exception values are defined as "False"
-                    continue # Skip the check
+            if exceptionValues[i] is not False and parameters[i] in exceptionValues[i]: # If the parameter is an exception value
                 raise ValueError(str(parameterNames[i])+" cannot be "+str(exceptionValues[i])+".") # Raise the error
         if type(parameters[i]) in [str,list,dict,tuple]: # If the parameter is a string, list, dictionary, or tuple
-            if len(parameters[i]) <= minValues[i]: # If the parameter's length is less than or equal to the minimum value
-                if not minValues[i]: # If the minimum value is defined as "False"
+            if len(parameters[i]) < minValues[i]: # If the parameter's length is less than the minimum value
+                if minValues[i] is False: # If the minimum value is defined as "False"
                     continue # Skip the check
                 raise ValueError(str(parameterNames[i])+" must have a length greater than or equal to "+str(minValues[i])+".") # Raise the error
-            if len(parameters[i]) >= maxValues[i]: # If the parameter's length is greater than or equal to the maximum value
-                if not maxValues[i]: # If the maximum value is defined as "False"
+            if len(parameters[i]) > maxValues[i]: # If the parameter's length is greater than the maximum value
+                if maxValues[i] is False: # If the maximum value is defined as "False"
                     continue # Skip the check
                 raise ValueError(str(parameterNames[i])+" must have a length less than or equal to "+str(maxValues[i])+".") # Raise the error
-            if parameters[i] in exceptionValues[i]: # If the parameter is an exception value
-                if not exceptionValues[i]: # If the exception values are defined as "False"
-                    continue # Skip the check
+            if exceptionValues[i] is not False and parameters[i] in exceptionValues[i]: # If the parameter is an exception value
                 raise ValueError(str(parameterNames[i])+"'s length cannot be "+str(exceptionValues[i])+".") # Raise the error
--- python/framework/numberFramework.py
+++ python/framework/numberFramework.py
@@ -1,101 +1,47 @@
-class LargeNumber:
-    import sys, math
-    global max_intsize, nextpower_maxint
-    max_intsize = sys.maxsize # Maximum size a integer can be 
-    nextpower_maxint = 10**math.ceil(math.log10(max_intsize)) # Next power of 10 after max_intsize
-    power10gap_maxint = nextpower_maxint - max_intsize # Gap between max_intsize and next power of 10
-    def __init__(self, component1,component2=[0,0],component3=[0,0]):
-        '''
-        System for going up to 1 Octodecillion (10^57). Takes advantage of int overflow amount and its next power of 10 (19) to store large numbers.
-        '''
-        if not type(component1) == list and not type(component2) == list and not type(component3) == list:
-            raise TypeError("Components must be lists [standard value, overflow to fill power of 10].")
-        if (not type(component1[0]) == float or not type(component1[0]) == int) and (not type(component1[1]) == float or not type(component1[10]) == int):
-            raise TypeError("Component 1's inputs must be a float or integer.")
-        if (not type(component2[0]) == int or not type(component2[1]) == int) and (not type(component2[0]) == int or not type(component2[1]) == int):
-            raise TypeError("Components 2 and 3's inputs must be integers.")
-        if component1[0] > LargeNumber.max_intsize:
-            raise ValueError("Component 1's standard value must be less than or equal to the maximum integer size.")
-        if component1[1] > LargeNumber.power10gap_maxint:
-            raise ValueError("Component 1's overflow value must be less than or equal to the maximum integer size.")
-        self.components = (component1, component2, component3)
-    
-    def compact(self):
-        # Convert the smaller numbers into the larger ones if needed, or vice versa if thats how the system ends up working
-        print("for later")
-
-    def __str__(self):
-        return self.number_str
-
-    def __add__(self, other):
-        if not isinstance(other, LargeNumber):
-            raise TypeError("Operands must be LargeNumber instances.")
-        return LargeNumber(str(int(self.number_str) + int(other.number_str)))
-
-    def __sub__(self, other):
-        if not isinstance(other, LargeNumber):
-            raise TypeError("Operands must be LargeNumber instances.")
-        return LargeNumber(str(int(self.number_str) - int(other.number_str)))
-
-    def __mul__(self, other):
-        if not isinstance(other, LargeNumber):
-            raise TypeError("Operands must be LargeNumber instances.")
-        return LargeNumber(str(int(self.number_str) * int(other.number_str)))
-
-    def __truediv__(self, other):
-        if not isinstance(other, LargeNumber):
-            raise TypeError("Operands must be LargeNumber instances.")
-        return LargeNumber(str(int(self.number_str) // int(other.number_str)))
-
-def abbreviate(number, decimals=3, digits=False):
-    import math, masterList
-    from generalFramework import onArgRaiseError
+def abbreviate(number, decimals=3, digits=False, scientific=False):
     '''
     Outputs a string representation of a number with a specified number of decimal places and/or digits.
 
     Args:
         number (int or LargeNumber): Input value.
         decimals (int): Number of decimal places to round to. (OPTIONAL)
-        digits (int): Number of digits to display from the number (Floors past ones place). (OPTIONAL)
+        digits (int or bool): Number of digits to display from the number (Floors past ones place). [Takes priority over decimals parameter] (OPTIONAL)
+        scientific (bool): Whether to use scientific notation. (OPTIONAL)
     '''
+    import math, masterList
+    from generalFramework import onArgRaiseError
 
-    onArgRaiseError((number,decimals,digits),("number","decimals","digits"),[[1,2,4],1,[1,3]],[False,0,False],[False,False,False],[False,False,False])
+    onArgRaiseError((number,decimals,digits,scientific),("number","decimals","digits","scientific"),[[1,2,4],1,[1,3],3],[False,0,1,False],[False,False,False,False],[False,False,False,False])
 
-    #if not type(number) == int and not type(number) == LargeNumber:
-    #    raise TypeError("Number must be an integer or LargeNumber instance.")
-    #if not type(decimals) == int:
-    #    raise TypeError("Decimals must be an integer.")
-    #if not type(digits) == int or not type(digits) == bool:
-    #    raise TypeError("Digits must be an integer or boolean.")
-    #if decimals < 0:
-    #    raise ValueError("Decimals must be a non-negative integer.")
-    
-    degree = math.floor(math.log10(number)/3)*3
+    if scientific:
+        if digits:
+            return f"{number:.{digits - 1}e}"
+        else:
+            return f"{number:.{decimals}e}"
 
-    if type(number) == int:
-        abbrevSymbol = masterList.abbreviations[degree]
-    number/=10**degree
-    number = math.floor(number*(10**decimals))/(10**decimals)
+    degree = 0
+
+    if number != 0:
+        degree = math.floor(math.log10(abs(number)) / 3) * 3
+        degree = min(degree, max(masterList.abbreviations.keys()))
+        number /= 10 ** degree
+
     if digits:
-        oldDigits = len(str(number)) - (1 if decimals > 0 else 0)-decimals
-        print(number)
-        print(oldDigits)
-        number = str(number)[:(digits+((1 if (digits // decimals > 0) else 0) if (decimals > 0) else 0))]
-        curDigits = len(str(number))
-        print(number)
-        print(curDigits)
-        number = float(number)*(10**(oldDigits - curDigits))
-        print(number)
-        print((digits+((1 if (digits // decimals > 0) else 0) if (decimals > 0) else 0)))
-    if decimals == 0:
-        number = int(number)
-    if digits and len(str(number)) - ((1 if (digits // decimals > 0) else 0) if (decimals > 0) else 0) > digits:
-        number = int(number)
-    return str(number)+abbrevSymbol
+        number = round(number, digits - int(math.floor(math.log10(abs(number)))) - 1)
+        if number.is_integer():
+            number = int(number)
+    else:
+        number = round(number, decimals)
+        if number.is_integer():
+            number = int(number)
 
-print(abbreviate(254254266260,5,4))
+    abbrev_symbol = masterList.abbreviations[degree]
+    return f"{number}{abbrev_symbol}"
 
-# Too lazy to fix this rn
-
-# And too lazy to fix this rn too
-# print(abbreviate(2525426260,-1)) works meaning generalFramework.py has an issue with minimum value checking
+# Example usage:
+print(abbreviate(2540000000000660205040000000000000042662602540000000000066020500000000000000000000000000000000000000000000426626025400000000000660205000000000000000000000000000000000000000000004266260254000000000006602050000000000000000000000000000000000000000000042662602540000000000066020500000000000000000000000000000000000000000000426626025400000000000660205000000000000000000000000000000000000000000004266260, 5, 3))  # Output: "254.254B"
+print(abbreviate(254254266260, 10))     # Output: "254.25B"
+#print(abbreviate(254254266260, 10,0))     # Throws error
+print(abbreviate(254254266260, digits=4))  # Output: "254.3B"
+print(abbreviate(2542542000066260, 5, scientific=True))  # Output: "2.54250e+15"
+print(abbreviate(2542542000066260, digits=4, scientific=True))  # Output: "2.542e+15"
