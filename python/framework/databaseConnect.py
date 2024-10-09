0b3d88bcbaa4f7960694b9f193f91c79584bb011
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-10-09 19:26:18 +0000

    soft cap, hard cap, and exponential functions through lambdas

--- python/framework/databaseConnect.py
+++ python/framework/databaseConnect.py
@@ -13,3 +13,41 @@ print(mydb)
 # We will use this to do databases rather than an API for google drive or smth
 # This is a snippet of code that connects to a database using mysql.connector
 # MySQL, so fun
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase") # Creates a Database (I think)
+
+# ----------------------------
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x) # Prints the databases
+# ^ Can be used to check if a database exists
+
+# ----------------------------
+
+# This would connect to the database "mydatabase" if it existed, otherwise you get an error
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))") # This creates a table if the code executes with no errors
+
+# ----------------------------
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+  print(x) # Prints the tables
+# ^ Can be used to check if a table exists
+
+# ----------------------------
+
+# Just use this website for more stuff: https://www.w3schools.com/python/python_mysql_create_table.asp
