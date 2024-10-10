b19abde73c126c69ddade4310b959e17984a128d
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-10-10 19:01:04 +0000

    mayhaps this will work?

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('trueIndex.html')
+    return render_template('index.html')
 
 @app.route('/get_value')
 def get_value():
