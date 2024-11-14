1cbc0319f804e23181914ca4cc4aad26cb240e50
Kai <kailunden9@gmail.com>
2024-11-14 03:25:30 +0000

    Fixing import errors before and after they happen (i can code :))

--- static/variables.js
+++ static/variables.js
@@ -1,3 +1,5 @@
 // This should be what it imported from the sheet, we might change this into a json file later.
 var Points = 0 
+export {Points}
 var ClickValue = 1 
+export {ClickValue}
--- static/visuals.js
+++ static/visuals.js
@@ -1,7 +1,7 @@
+import { Points } from "./variables";
 function GameTick() {
     document.getElementById('points').innerHTML = "You have:"+(Points)+" Dilyan Points";
     //add anything that should be constantly running 
 
 }
-
 setInterval(GameTick, 10);
