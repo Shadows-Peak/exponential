c4b455a54d3bc7f381689c1167a21bd92c57807a
Kai <kailunden9@gmail.com>
2024-11-14 03:14:22 +0000

    i swear im going to crash out if this is the issue

--- static/visuals.js
+++ static/visuals.js
@@ -1,5 +1,7 @@
 function GameTick() {
     document.getElementById('points').innerHTML = "You have:"+(Points)+" Dilyan Points";
-    //add anything that should be constantly running :)
+    //add anything that should be constantly running 
+
 }
-setInterval(GameTick, 10)
+
+setInterval(GameTick, 10);
