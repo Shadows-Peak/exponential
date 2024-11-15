8de8f61baa887af3d59bed828f63d48216549c40
Kai <kailunden9@gmail.com>
2024-11-15 03:12:50 +0000

    more fixing

--- static/framework/numberFramework.js
+++ static/framework/numberFramework.js
@@ -1,4 +1,5 @@
-export default class LargeNumber {
+
+/*export default class LargeNumber {
     constructor(value) {
         this.value = BigInt(value);
     }
@@ -30,3 +31,4 @@ const num1 = new LargeNumber('1' + '0'.repeat(603));
 const num2 = new LargeNumber('2');
 const result = num1.add(num2);
 console.log(result.toString()); // Outputs a number with 603 zeros followed by 2
+*/
--- static/main.js
+++ static/main.js
@@ -1,7 +1,7 @@
 //import LargeNumber from "./framework/numberFramework.js";
 //import { ClickValue } from "./variables.js";
-const num1 = new LargeNumber('1' + '0'.repeat(603));
-const num2 = new LargeNumber('2');
+//const num1 = new LargeNumber('1' + '0'.repeat(603));
+//const num2 = new LargeNumber('2');
 const result = num1.add(num2);
 console.log(result.toString()); // Outputs a number with 603 zeros followed by 2
 
