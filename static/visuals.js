f9b2029c19c95b91ebd3aa2e1df1e1836425521c
Kai <kailunden9@gmail.com>
2024-11-18 21:01:05 +0000

    more testing

--- static/visuals.js
+++ static/visuals.js
@@ -2,8 +2,11 @@ function GameTick() {
     document.getElementById('points').innerHTML = "You have:" + (Points) + " Dilyan Points";
     //add anything that should be constantly running\
     roomids = document.querySelectorAll('[room-id]');
-    document.getElementById('value-container').innerHTML = roomids
-
+    iteration = 0 
+    roomids.forEach(node => {
+        interation += 1
+    });
+    document.getElementById('value-container').innerHTML = iteration
 }
 setInterval(GameTick, 10);
 
