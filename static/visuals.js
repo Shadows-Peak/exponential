f61bdde6ae412eb23a3bee1f7f93e22e292951ba
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-12-11 03:37:59 +0000

    test if 'points' is present

--- static/visuals.js
+++ static/visuals.js
@@ -1,5 +1,7 @@
 function GameTick() {
-    document.getElementById('points').innerHTML = "You have:" + (Points) + " Dilyan Points";
+    if (document.getElementById('points')) {
+        document.getElementById('points').innerHTML = "You have:" + (Points) + " Dilyan Points";
+    }
     //add anything that should be constantly running\
     roomids = document.querySelectorAll('[data-room-id]');
     console.log(Points)
