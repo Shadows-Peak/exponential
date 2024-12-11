a7e0b3196d32c34ca58030bc42c9d69399c53a86
Kai <kailunden9@gmail.com>
2024-12-11 20:42:55 +0000

    testing123

--- static/visuals.js
+++ static/visuals.js
@@ -2,6 +2,7 @@ function GameTick() {
     if (document.getElementById('points')) {
         document.getElementById('points').innerHTML = "You have:" + (Points) + " Dilyan Points";
     }
+    document.getElementById('version').innerHTML = (output);
     //add anything that should be constantly running\
     roomids = document.querySelectorAll('[data-room-id]');
     //sconsole.log(Points)
