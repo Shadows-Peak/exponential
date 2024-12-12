24791c151e73a00f7024fc3fdc980acd866d6be9
Kai <kailunden9@gmail.com>
2024-12-12 19:55:29 +0000

    help

--- static/database.js
+++ static/database.js
@@ -1,13 +1,22 @@
 const {GristDocAPI} = require('grist-api');
-const DOC_URL = "https://docs.getgrist.com/9KC8xj6xEYzA/docs";
-const API_KEY = "d398ddbce150c59c00a039c76b4bd24d6e35ba5b"
-const api = new GristDocAPI(GristDocAPI);
-new GristDocAPI(DOC_URL, {apiKey: API_KEY})
-function get_pass(username) {
-    api.fetchTable(Table1)
 
+// Put here the URL of your document.
+const DOC_URL = "https://docs.getgrist.com/9KC8xj6xEYzA/docs";
+new GristDocAPI(DOC_URL, {apiKey: 'd398ddbce150c59c00a039c76b4bd24d6e35ba5b'})
+async function main() {
+  const api = new GristDocAPI(DOC_URL);
+  // Add some rows to a table
+  await api.addRecords('Food', [
+    {Name: 'eggs', AmountToBuy: 12},
+    {Name: 'beets', AmountToBuy: 1},
+  ]);
+
+  // Fetch all rows.
+  const data = await api.fetchTable('Food');
+  alert(data);
+
+  // Sync data by a key column.
+  await api.syncTable('Food', [{Name: 'eggs', AmountToBuy: 0}], ['Name']);
 }
-const output = api.fetchTable(Table1);
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('version').innerHTML = "version " + (output) + ".";
-});
+
+main();
