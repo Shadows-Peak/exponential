7fe40a96261a1196ed9d5dbae40861e60c0cfb34
Kai <kailunden9@gmail.com>
2024-12-13 19:36:25 +0000

    heheheha

--- static/database.js
+++ static/database.js
@@ -1,1 +1,23 @@
 Airtable.configure({ apiKey: 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933' })
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'YOUR_SECRET_API_TOKEN'}).base('appXXbVu5p4uSKViT');
+
+base('Table 1').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
