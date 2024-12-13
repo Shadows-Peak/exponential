a25d96cb24d37fb2b862fcd16dc99aa46c8856e6
Kai <kailunden9@gmail.com>
2024-12-13 16:32:00 +0000

    these both are prob okay so...

--- static/database.js
+++ static/database.js
@@ -21,3 +21,5 @@ async function main() {
 */
 
 const {GristDocAPI} = require('grist-api');
+const DOC_URL = "https://docs.getgrist.com/9KC8xj6xEYzA/docs";
+new GristDocAPI(DOC_URL, {apiKey: 'd398ddbce150c59c00a039c76b4bd24d6e35ba5b'})
