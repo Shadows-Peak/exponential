341e882a649f58248d5014a30a40d19f068f0af9
Kai <kailunden9@gmail.com>
2024-12-14 02:55:55 +0000

    this is really silly :O :)

--- static/database.js
+++ static/database.js
@@ -1,6 +1,6 @@
 const API_KEY = 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933';
-const BASE_ID = 'your_base_id';
-const TABLE_NAME = 'your_table_name';
+const BASE_ID = 'appXXbVu5p4uSKViT';
+const TABLE_NAME = 'userdata';
 const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
 async function fetchAirtableData() {
     const response = await fetch(url, {
