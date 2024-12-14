a076f26460afe1a7baf0a6bce95cc47a54f52dd5
Kai <kailunden9@gmail.com>
2024-12-14 19:02:21 +0000

    oopsie

--- static/database.js
+++ static/database.js
@@ -1,6 +1,6 @@
 const API_KEY = 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933';
 const BASE_ID = 'appXXbVu5p4uSKViT';
-const TABLE_NAME = 'userdata';
+const TABLE_NAME = 'logins';
 const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
 async function fetchAirtableData() {
     const response = await fetch(url, {
