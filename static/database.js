8bf766eaab5e95bf9e2857fc55eda7795611f72f
Kai <kailunden9@gmail.com>
2024-12-14 00:29:34 +0000

    let him/her/them/it idk cook

--- static/database.js
+++ static/database.js
@@ -1,22 +1,15 @@
-Airtable.configure({ apiKey: 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933' })
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933'}).base('appXXbVu5p4uSKViT');
-base('logins').select({
-    // Selecting the first 3 records in Grid view:
-    maxRecords: 3,
-    view: "Grid view"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('Username'));
+const API_KEY = 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933';
+const BASE_ID = 'your_base_id';
+const TABLE_NAME = 'your_table_name';
+const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+async function fetchAirtableData() {
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${API_KEY}`
+        }
     });
+    const data = await response.json();
+    console.log(data.records); // Access your Airtable records here
+}
 
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
-
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
+fetchAirtableData();
