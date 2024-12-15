dce60ddccad0c1d0a30986c26e083e47b670c3bb
Kai <kailunden9@gmail.com>
2024-12-15 19:12:47 +0000

    fixing the previous testing

--- static/database.js
+++ static/database.js
@@ -12,4 +12,4 @@ async function fetchAirtableData() {
     console.log(data); // Failed to load resource: the server responded with a status of 403 () -> {"error":{"type":"AUTHENTICATION_REQUIRED","message":"Authentication required"}}
     return data
 }
-alert(fetchAirtableData);
+alert(fetchAirtableData());
