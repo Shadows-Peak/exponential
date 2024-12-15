1fd2dd35af0fe3a2f8e9a4485cfd441d7e38fd3e
Kai <kailunden9@gmail.com>
2024-12-15 20:40:02 +0000

    postman workflow moment (this will no work)

--- static/database.js
+++ static/database.js
@@ -1,23 +1,15 @@
-const API_KEY = 'patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933';
-const BASE_ID = 'appXXbVu5p4uSKViT';
-const TABLE_NAME = 'logins';
-const url = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
-async function fetchAirtableData() {
-    const response = await fetch(url, {
-        headers: {
-            Authorization: `Bearer ${API_KEY}`
-        }
-    });
-    const data = JSON.stringify((await response.json()) , null , 2) // Failed to load resource: the server responded with a status of 403 () -> {"error":{"type":"AUTHENTICATION_REQUIRED","message":"Authentication required"}}
-    return data
-}
-console.log(data);
+const myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
+myHeaders.append("Cookie", "brw=brwtcGK1QAbqebbws; brwConsent=opt-in; AWSALBTG=jxag47/tC1Fkw2Yl/l2+Jqwucby7OsAjd1D5qCTkf+gFRFWSc/+zaZ8o6AfMR/8hmqk0GhB2ZCIhOD1ErlVWdeZB43VvJcid+oll+ZIEYWeUvQS4dc+xlFxsKM6jnKvjadHV+AfMYp6cXRLAl4WYlpzPHRatzhR0BK16LWk/vU3t8VTO6W0=; AWSALBTGCORS=jxag47/tC1Fkw2Yl/l2+Jqwucby7OsAjd1D5qCTkf+gFRFWSc/+zaZ8o6AfMR/8hmqk0GhB2ZCIhOD1ErlVWdeZB43VvJcid+oll+ZIEYWeUvQS4dc+xlFxsKM6jnKvjadHV+AfMYp6cXRLAl4WYlpzPHRatzhR0BK16LWk/vU3t8VTO6W0=");
 
-base('logins').select({
-    view: 'Grid view'
-}).firstPage(function(err, records) {
-    if (err) { console.error(err); return; }
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('Username'));
-    });
-});
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
+};
+
+fetch("https://api.airtable.com/v0/appXXbVu5p4uSKViT/logins", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+
