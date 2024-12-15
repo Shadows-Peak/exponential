32d649a62a0425fb67a77082850b6e0e17d570b3
Kai <kailunden9@gmail.com>
2024-12-15 22:32:12 +0000

    cooking off the charn rn

--- static/main.js
+++ static/main.js
@@ -106,9 +106,13 @@ function submitRun(event) {
 
     if (form.id === 'signup-form') {
         console.log('Sign Up Form Data:', JSON.stringify(data));
+        console.log(data[0])
+        console.log(data[1])
         // Add your sign-up logic here
     } else if (form.id === 'login-form') {
-        console.log('Login Form Data:', data);
+        console.log('Login Form Data:', JSON.stringify(data));
+        console.log(data[0])
+        console.log(data[1])
 
         // Load game
         gameLoad();
