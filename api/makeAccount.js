f78660f8d89c35f69bf87a96336553df2f0d74c1
Kai <kailunden9@gmail.com>
2024-12-16 02:31:43 +0000

    testing

--- api/makeAccount.js
+++ api/makeAccount.js
@@ -1,19 +1,17 @@
 // you thought this was going to be easy, huh
+import { json } from 'micro';
 export default async function(req, res) {
     const API_Key = process.env.API_KEY;
-    if (!apiKey){
+    if (!API_KEY){
         return res.status(500).json({message: "no api key :("})
     }
-
-    //logic :)
-    /*
+    const data = await json(req)
+    const {username, password } = data;
     const myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer " + API_KEY);
+myHeaders.append("Authorization", API_KEY);
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Cookie", "brw=brwtcGK1QAbqebbws; brwConsent=opt-in; AWSALBTG=ORWyx0GOeOeFC4Htx8qmUOyxTWMglIFRlmHSFCsHUGQjDt9U02Tsw6NZqNS6iaFy1dR7320woQicQisGMFqnU2DH0HrMtl+kfmVdeHCVa8iAkaYI1wBHVyDFizsxs748OPKKssThtiNNxJu3fme8XNO06csnnMzjj6kUDcXMt+qOHsxxHpo=; AWSALBTGCORS=ORWyx0GOeOeFC4Htx8qmUOyxTWMglIFRlmHSFCsHUGQjDt9U02Tsw6NZqNS6iaFy1dR7320woQicQisGMFqnU2DH0HrMtl+kfmVdeHCVa8iAkaYI1wBHVyDFizsxs748OPKKssThtiNNxJu3fme8XNO06csnnMzjj6kUDcXMt+qOHsxxHpo=");
-const data = await json(req);
-const username = data['username']
-const password = data['password']
+myHeaders.append("Cookie", "brw=brwtcGK1QAbqebbws; brwConsent=opt-in; AWSALBTG=05EdyotyuE+/rdSRD+10ASSNIr4LQdBvhypgba7SRyIq6cCVn45d0C5PTOl4+YKC1Hnlwc54PGBe7HgSJgGxdN3cRVah/j9XrB5C1ij6VhI43rR4M1N6oNyPbRGfJgs3S6P9T8Uk7kp/w2mKtoJ0VboBl90PkhRaOkgxlC0cwkeUgseipug=; AWSALBTGCORS=05EdyotyuE+/rdSRD+10ASSNIr4LQdBvhypgba7SRyIq6cCVn45d0C5PTOl4+YKC1Hnlwc54PGBe7HgSJgGxdN3cRVah/j9XrB5C1ij6VhI43rR4M1N6oNyPbRGfJgs3S6P9T8Uk7kp/w2mKtoJ0VboBl90PkhRaOkgxlC0cwkeUgseipug=");
+
 const raw = JSON.stringify({
   "records": [
     {
@@ -25,16 +23,17 @@ const raw = JSON.stringify({
   ]
 });
 
-    const requestOptions = {
+const requestOptions = {
   method: "POST",
   headers: myHeaders,
   body: raw,
   redirect: "follow"
-    };
-    fetch("https://api.airtable.com/v0/appXXbVu5p4uSKViT/logins", requestOptions)
+};
+
+fetch("https://api.airtable.com/v0/appXXbVu5p4uSKViT/logins", requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
-*/
+
     res.status(200).json({ message:'i gotchu boo'})
 }
