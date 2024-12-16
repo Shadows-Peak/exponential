69849a257fd54a36bbc66d6cabcfde91cdc0a5fc
Kai <kailunden9@gmail.com>
2024-12-16 03:13:05 +0000

    back to baiscs

--- api/makeAccount.js
+++ api/makeAccount.js
@@ -1,17 +1,6 @@
 import { json } from 'micro';
 
 export default async function(req, res) {
-  // Get the API key from environment variables
-  const API_Key = process.env.API_KEY;
-  
-  // If API_KEY is not set, return a 500 error
-  if (!API_Key) {
-    return res.status(500).json({ message: "no API key :(" });
-  }
-
-  try {
-
-
     const myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer patFPAzk3Ni4jtL7K.8bdcda86e17b32bd177f9ab25661e401e4454a8e4a2401a267c36b67e94ea933");
     myHeaders.append("Content-Type", "application/json");
@@ -39,10 +28,4 @@ export default async function(req, res) {
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.error(error));
-
-  } catch (error) {
-    // Catch any errors and log them
-    console.error("Error creating account:", error);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
 }
