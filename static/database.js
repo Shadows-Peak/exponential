1081d8b9eeac9e9e49da74614b18939cb1dceeca
Kai <kailunden9@gmail.com>
2024-12-20 00:42:11 +0000

    yap

--- static/database.js
+++ static/database.js
@@ -1,20 +1,12 @@
-async function uploadLoginData(username, password) {
-  try {
-    const response = await fetch('https://exponential-psi.vercel.app/api/makenewaccount', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    });
+async function createNewAccount(username, password) {
+  const response = await fetch('https://exponential-psi.vercel.app/api/makenewaccount', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
 
-    const data = await response.json();
-    if (response.ok) {
-      console.log('Success:', data);
-    } else {
-      console.error('Error:', data.message);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  const data = await response.json();
+  return data;
 }
