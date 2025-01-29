2d689ca7baa40130d9cbaa306f073b011eb34aa1
Kai <kailunden9@gmail.com>
2025-01-29 19:04:03 +0000

    okay so may have been using the wrong function xD

--- static/database.js
+++ static/database.js
@@ -1,5 +1,5 @@
 async function createNewAccount(username, password) {
-  const response = await fetch('https://exponential-psi.vercel.app/api/makenewaccount', {
+  const response = await fetch('https://dlie2.netlify.app/.netlify/functions/hello-world', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
