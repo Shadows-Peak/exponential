a43a7018283e7c9953d6c0c8ad0d5e1fc3986cf2
Kai <kailunden9@gmail.com>
2025-02-13 21:40:20 +0000

    signUP should work now

--- static/database.js
+++ static/database.js
@@ -1,13 +1,34 @@
-async function createNewAccount(username, password) {
-  const response = await fetch('https://dlie2.netlify.app/.netlify/functions/hello-world', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body:{ "username":username, "password":password },
-  })
-  .then(responce => responce.json);
+async function signUp(username, password) {
+  const apiKey = "patkkZrsUMphPMHSV.e0f6dc672fa00f478e351863f853c0e027cb425e5572b6c5806e207b26affa8f"
+  const baseId = "appXXbVu5p4uSKViT"
+  const tableName = "logins"
+  const url = `https://api.airtable.com/v0/${baseId}/${tableName}`
+  const data = {
+      "records": [
+          {
+              "fields": {
+                  "username": username,
+                  "password": password
+              }
+          }
+      ]
+  }
+  try {
+      const response = await fetch(url, {
+          method: "POST",
+          headers: {
+              authorization: `Bearer ${apiKey}`,
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify(data)
+      })
+      return response.json()
+      
 
-  const data = await response.json();
-  alert(data)
+}   catch (error) {
+      console.error("Error signing up:", error)
+      return null
+  }  
 }
+// Expose the function globally so main.js can use it.
+window.signUp = signUp;
