11b0bdb3a865e26044ed02e56a6bbacbc72bbec4
Kai <kailunden9@gmail.com>
2025-03-28 18:57:07 +0000

    Add logging for form submission data in main.js

--- static/main.js
+++ static/main.js
@@ -80,6 +80,7 @@ async function submitRun(event) {
     const form = event.target;
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
+    console.log('Form submitted:', form.id, data, JSON.stringify(data, undefined, 2), form, formData);
 
     if (form.id === 'signup-form') {
         console.log('Sign Up Form Data:', JSON.stringify(data, undefined, 2));
