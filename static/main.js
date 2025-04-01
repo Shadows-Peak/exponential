71f0c8f890f0566a699ce96b1e9edbb203c20248
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2025-04-01 21:32:10 +0000

    forgot something

--- static/main.js
+++ static/main.js
@@ -433,7 +433,7 @@ function gameLoad() {
                     // This ensures we don't package more than we have queued
                     shipmentsQueued -= shipmentsToPackage;
                     // Animate the package to the shipping station
-                    animatePackageToShippingStation();
+                    animatePackageToShippingStation(shipmentsToPackage);
                     return; // Exit the function after packaging the difference
                 }
                 let shipmentsToPackage = shipmentsQueued;
