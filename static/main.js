426ae9012a0de4af5dd3071a8614019d83f232bc
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2025-04-07 22:23:48 +0000

    tests in place

--- static/main.js
+++ static/main.js
@@ -334,6 +334,9 @@ function gameLoad() {
                     // Check if shipmentsLoaded is at max capacity
                     if (shipmentsLoaded >= maxShipments || willFillorIsFull) {
                         // Animate the package to the waiting position
+                        alert("index Variable: "+index);
+                        alert("curWaiting Variable: "+curWaiting);
+                        alert("Combined: "+(curWaiting + index + 1));
                         const waitingPositionX = processUnitRect.left + (shippingStationRect.left - processUnitRect.left) * (curWaiting + index + 1) / (curWaiting + index + 2);
                         const waitingPositionY = processUnitRect.top + (shippingStationRect.top - processUnitRect.top) * (curWaiting + index + 1) / (curWaiting + index + 2);
 
