2a6a7465280ff443bff8a6f6a42c6555ea3d6755
Kai <kailunden9@gmail.com>
2025-04-11 20:11:46 +0000

    trying spliting functions into different file to make main less cluttered

--- static/main.js
+++ static/main.js
@@ -140,34 +140,6 @@ async function submitRun(event) {
     }
 }
 
-function menuLoad() {
-    // HTML Load
-    fetch('./templates/menu.html')
-    .then(response => response.text())
-    .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        document.body.innerHTML = doc.body.innerHTML;
-
-        // Event Listeners
-
-        try{
-            document.getElementById('signUp').removeEventListener('click',signUpRun);
-            document.getElementById('login').removeEventListener('click',loginRun);
-            document.getElementById('backButton').removeEventListener('click',backButtonRun);
-            document.removeEventListener('submit',submitRun);
-        } catch(error) {
-            console.log("No event listeners to remove")
-        }
-
-        document.getElementById('signUp').addEventListener('click', signUpRun);
-        document.getElementById('login').addEventListener('click', loginRun);
-        document.addEventListener('submit', submitRun);
-    })
-    .catch(error => alert(error));
-    //.catch(error => console.error('Error loading menu.html:', error));
-}
-
 function gameLoad() {
     // HTML Load
     fetch('./templates/game.html')
@@ -543,6 +515,7 @@ async function forumLoad() {
         // Load posts from Airtable
         const posts = await loadPosts(); // Ensure loadPosts is defined and works correctly
         console.log('Posts:', JSON.stringify(posts, undefined, 2));
+        
 
         // Handle stylesheet dynamically
         let link = document.getElementById("mainStyleSheet");
